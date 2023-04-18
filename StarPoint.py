e4360bf99edc279cdc09e2ba74e92df6684e6d15
JosefTheKing17 <yosimami50@gmail.com>
2023-04-18 19:03:48 +0300

    Need to implement brightness function

--- StarPoint.py
+++ StarPoint.py
@@ -1,9 +1,14 @@
 class StarPoint:
-    def __int__(self, _x, _y, _radius, _brightness):
+    def __init__(self, _id, _x, _y, _radius, _brightness):
+        self._star_id = _id
         self._x = _x
         self._y = _y
         self._radius = _radius
         self._brightness = _brightness
 
     def __str__(self):
-        return f"x: {self._x} y: {self._y} r: {self._radius} b: {self._brightness}"
+        return f"StarPoint: id: {self._star_id} x: {self._x} y: {self._y} r: {self._radius} b: {self._brightness}"
+
+    def calc_brightness(self):
+        return None
+
--- main.py
+++ main.py
@@ -2,67 +2,48 @@ from time import sleep
 import numpy as np
 import cv2
 
+from StarPoint import StarPoint
+
+original_img = cv2.imread('stars.jpg')
+mask = np.zeros_like(original_img[:, :, 0])
 img = cv2.imread('stars.jpg', cv2.IMREAD_GRAYSCALE)
+(width, height) = img.shape
 
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, binary_img = cv2.threshold(img, 175, 235, cv2.THRESH_BINARY)
-binary_img = cv2.resize(binary_img, (img.shape[1], img.shape[0]))
-binary_img = cv2.imread(binary_img, cv2.)
+blackImage = np.ones((width, height), dtype=np.uint8)
+ret, binary_img = cv2.threshold(img, 140, 235, cv2.THRESH_BINARY)
+binary_img = cv2.blur(binary_img, (5, 5))
 
-gray = cv2.cvtColor(binary_img, cv2.COLOR_BGR2GRAY)
+# cv2.namedWindow('binary_image', cv2.WINDOW_NORMAL)
+# cv2.imshow('binary_image', binary_img)
 
-# binary_img = cv2.resize(binary_img, (img.shape[1], img.shape[0]))
+stars = []
+circles = cv2.HoughCircles(binary_img, cv2.HOUGH_GRADIENT, 8, 13, param1=5, param2=10, minRadius=0, maxRadius=10)
 
-print("finished")
+circles = np.uint16(np.around(circles))
+star_id = 1
+
+for (x, y, r) in circles[0, :]:
+    cv2.circle(mask, (x, y), r, 1, -1)
+    masked_img = cv2.bitwise_and(img, img, mask=mask)
+    brightness = np.mean(masked_img[mask == 1])
+    stars.append(StarPoint(star_id, x, y, r, brightness))
+    # draw the outer circle
+    cv2.circle(blackImage, (x, y), r + 10, (255, 255, 255), 2)
+    # draw the number
+    cv2.putText(blackImage, f"{star_id}", (x - 10, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0, 255), 2)
+    # draw the center of the circle
+    cv2.circle(blackImage, (x, y), 1, (255, 255, 255), 3)
+    star_id += 1
+
+for star in stars:
+    print(star)
 
 # show image to screen
-cv2.namedWindow('thresh', cv2.WINDOW_NORMAL)
-cv2.imshow('thresh', gray)
-
-# wait for key to close
+# cv2.namedWindow('binary_image', cv2.WINDOW_NORMAL)
+# cv2.imshow('binary_image', cv2.WINDOW_NORMAL)
+cv2.namedWindow('original', cv2.WINDOW_NORMAL)
+cv2.imshow("original", original_img)
+cv2.namedWindow('identified', cv2.WINDOW_NORMAL)
+cv2.imshow('identified', blackImage)
 cv2.waitKey(0)
-
-# circles = cv2.HoughCircles(image=binary_img, method=cv2.HOUGH_GRADIENT, dp=0.9,
-#                            minDist=80, param1=110, param2=39, maxRadius=70)
-# apply a blur using the median filter
-# img = cv2.medianBlur(img, 5)
-
-# Resize if needed:
-# cv2.namedWindow('thresh', cv2.WINDOW_NORMAL)
-
-# img = cv2.GaussianBlur(img, (9, 9), 0)
-
-# thresholding
-# ret, binary_img = cv2.threshold(img, 170, 255, cv2.THRESH_BINARY)
-#
-# cv2.waitKey(0)
-#
-# circles = cv2.HoughCircles(binary_img, cv2.HOUGH_GRADIENT, 2, 3, param1=5, param2=10, minRadius=0, maxRadius=10)
-#
-# circles = np.uint16(np.around(circles))
-# colored_img = cv2.cvtColor(binary_img, cv2.COLOR_GRAY2BGR)
-#
-# for i in circles[0, :]:
-#     print(i)
-#     # draw the outer circle
-#     cv2.circle(colored_img, (i[0], i[1]), 12, (255, 255, 255), 2)
-#     # draw the center of the circle
-#     cv2.circle(colored_img, (i[0], i[1]), 1, (255, 255, 255), 3)
-#
-#     # calculate the brightness:
-#     circle_center = (i[0], i[1])
-#     circle_radius = i[2]
-#
-# # Create a binary mask for the circle
-# mask = np.zeros_like(img, dtype=np.uint8)
-# # rr, cc = skimage.draw.circle(circle_center[0], circle_center[1], circle_radius)
-# np.where()
-# mask[rr, cc] = 1
-#
-# # Extract the pixels inside the circle
-# pixels_in_circle = img * mask
-#
-# # Calculate the average pixel value inside the circle
-# average_pixel_value = np.mean(pixels_in_circle[np.nonzero(pixels_in_circle)])
-#
-# print(f"The average pixel value inside the circle is {average_pixel_value}")
+cv2.destroyAllWindows()
